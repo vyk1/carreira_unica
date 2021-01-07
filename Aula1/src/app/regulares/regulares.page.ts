6b4abdb2580406c74038a85204c2552779a7db09
unknown <victoriabotelho14@gmail.com>
2021-01-07 14:16:54 -0300

    Feat: img

--- Aula1/src/app/regulares/regulares.page.ts
+++ Aula1/src/app/regulares/regulares.page.ts
@@ -7,8 +7,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./regulares.page.scss'],
 })
 export class RegularesPage implements OnInit {
+  items: object[]
 
-  constructor(public router: Router) { }
+  constructor(public router: Router) {
+    this.items = [
+      { url: 'https://www.geeksforgeeks.org/difference-between-dfa-and-nfa/?ref=rp', title: 'GeeksForGeeks', key: '1*' },
+      { url: 'https://ths.rwth-aachen.de/wp-content/uploads/sites/4/regular_exercises_ws1718.pdf', title: 'Exercício' },
+    ]
+  }
 
   navigateToAF() {
     this.router.navigate(['regulares/automatos-finitos'], {})
