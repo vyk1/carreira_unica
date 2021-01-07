630acb82ee724b4ce11ffb019b648e4d54e12e15
unknown <victoriabotelho14@gmail.com>
2021-01-07 12:55:32 -0300

    Feat: AF

--- Aula1/src/app/regulares/automatos-finitos/automatos-finitos.page.ts
+++ Aula1/src/app/regulares/automatos-finitos/automatos-finitos.page.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-automatos-finitos',
   templateUrl: './automatos-finitos.page.html',
   styleUrls: ['./automatos-finitos.page.scss'],
 })
-export class AutomatosFinitosPage implements OnInit {
+export class AutomatosFinitosPage {
 
+  @ViewChild(IonContent, { static: false }) content: IonContent;
   constructor() { }
 
+  scrollToElement(elementId: string) {
+    let yOffset = document.getElementById(elementId).offsetTop;
+    console.log(yOffset)
+    this.content.scrollToPoint(0, yOffset);
+  }
   ngOnInit() {
   }
 
